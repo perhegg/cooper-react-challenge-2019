2b00b2070b116a5ac2df13f0943400d6dd3aabe2
nevroje <joakim.berglind@gmail.com>
2019-03-06 15:12:12 +0100

    refactored app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DisplayCooperResult from './Components/DisplayCooperResult';
+import InputFields from "./Components/InputFields";
+import LoginForm from './Components/LoginForm';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      distance: '',
+      gender: 'female',
+      age: '',
+      renderLoginForm: false
+    }
+  }
+
+  onChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
   render() {
+    let renderLogin
+
+    if (this.state.renderLoginForm === true) {
+      renderLogin = (
+        <LoginForm />
+      )
+    } else {
+      renderLogin = (
+        <button id="login" onClick={() => this.setState({ renderLoginForm: true })}>Login</button>
+      )
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <InputFields 
+          inputChangeHandler={this.onChange.bind(this)}
+        />
+        <DisplayCooperResult
+          distance={this.state.distance}
+          gender={this.state.gender}
+          age={this.state.age}
+        />
+        {renderLogin}
+
       </div>
     );
   }
