e4f4bdc8758823092355ab1ba79899625f308c53
nevroje <joakim.berglind@gmail.com>
2019-03-07 14:38:40 +0100

    added displayperformancedata component , refactored apps.js and modules retrieve data tests green

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import DisplayCooperResult from './Components/DisplayCooperResult';
 import InputFields from "./Components/InputFields";
 import LoginForm from './Components/LoginForm';
 import { authenticate } from './Modules/Auth';
+import DisplayPerformanceData from './Components/DisplayPerformanceData'
 
 
 class App extends Component {
@@ -22,7 +23,11 @@ class App extends Component {
     }
   }
   entryHandler() {
-    this.setState({ entrySaved: true });
+    this.setState({ entrySaved: true, updateIndex: true });
+  }
+
+  indexUpdated() {
+    this.setState({ updateIndex: false });
   }
 
   onChange(event) {
@@ -52,9 +57,21 @@ class App extends Component {
       renderLogin = (
         <p>Hi {user}</p>
       )
-      performanceDataIndex = (
-        <button id="show-index" onClick={() => this.setState({ renderIndex: true })}>Show past entries</button>
-      )
+      if (this.state.renderIndex === true) {
+        performanceDataIndex = (
+          <>
+            <DisplayPerformanceData
+              updateIndex={this.state.updateIndex}
+              indexUpdated={this.indexUpdated.bind(this)}
+            />
+            <button onClick={() => this.setState({ renderIndex: false })}>Hide past entries</button>
+          </>
+        )
+      } else {
+        performanceDataIndex = (
+          <button id="show-index" onClick={() => this.setState({ renderIndex: true })}>Show past entries</button>
+        )
+      }
     } else {
       if (this.state.renderLoginForm === true) {
         renderLogin = (
